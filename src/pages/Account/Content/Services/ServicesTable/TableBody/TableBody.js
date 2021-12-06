8a078645f4b46152927b4c6407ee1030b15cc73c
Hossam <56669856+HOSS11H@users.noreply.github.com>
2021-12-06 11:01:07 +0200

    Changing Status Style In Table

--- src/pages/Account/Content/Services/ServicesTable/TableBody/TableBody.js
+++ src/pages/Account/Content/Services/ServicesTable/TableBody/TableBody.js
@@ -73,7 +73,7 @@ const EnhancedTableBody = props => {
                             </TableEmployees>
                         </TableCell>
                         <TableCell align="center">
-                            <TableStatus>{row.status}</TableStatus>
+                            <TableStatus className={row.status}>{row.status}</TableStatus>
                         </TableCell>
                         <TableCell align="center">
                             <Actions edit remove view
