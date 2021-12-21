74f4389bc5d38a0a2a5bf08a457b03c2d2d65bbc
Hossam <56669856+HOSS11H@users.noreply.github.com>
2021-12-21 13:44:17 +0200

    Edit & Create  REfactor

--- src/pages/Account/Content/Expenses/ExpenseCategories/ExpenseCategoriesTable/EditModal/EditModal.js
+++ src/pages/Account/Content/Expenses/ExpenseCategories/ExpenseCategoriesTable/EditModal/EditModal.js
@@ -6,89 +6,12 @@ import ThemeContext from '../../../../../../../store/theme-context'
 import { CustomModal } from '../../../../../../../components/UI/Modal/Modal';
 import { Grid } from '@mui/material';
 import TextField from '@mui/material/TextField';
-import ReactSelect from 'react-select';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-
-import DateAdapter from '@mui/lab/AdapterDateFns';
-import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
-
-import { Editor } from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { EditorState, convertToRaw, ContentState } from 'draft-js';
-import draftToHtml from 'draftjs-to-html';
-import htmlToDraft from 'html-to-draftjs';
-import { connect } from 'react-redux';
-import { fetchLocations, fetchServicesByLocation } from '../../../../../../../store/actions/index';
 import ValidationMessage from '../../../../../../../components/UI/ValidationMessage/ValidationMessage';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import { format } from 'date-fns';
-import axios from '../../../../../../../utils/axios-instance';
 
 
 const CustomTextField = styled(TextField)`
     width: 100%;
 `
-
-
-const EditorWrapper = styled.div`
-    .rdw-editor-wrapper {
-        border-radius: 20px;
-        border: 1px solid;
-        border-color: ${({ theme }) => theme.palette.divider};
-        margin-bottom: 20px;
-    }
-    .rdw-editor-toolbar {
-        border: 0;
-        border-bottom: 1px solid;
-        border-color: ${({ theme }) => theme.palette.divider};
-        border-radius: 20px 20px 0 0;
-        padding: 20px;
-        background-color: ${({ theme }) => theme.palette.action.hover};
-    }
-    .rdw-editor-main {
-        padding: 0 20px 20px;
-    }
-    .rdw-dropdown-carettoopen {
-        position: relative;
-        top: auto;
-        right: auto;
-        left: auto;
-    }
-    .rdw-dropdown-carettoclose {
-        position: relative;
-        top: auto;
-        right: auto;
-        left: auto;
-    }
-    .rdw-dropdown-selectedtext {
-        color: ${({ theme }) => theme.palette.common.black};
-        justify-content: space-between;
-    }
-    .rdw-dropdown-optionwrapper {
-        color: ${({ theme }) => theme.palette.common.black};
-    }
-    .rdw-embedded-modal {
-        background-color: ${({ theme }) => theme.palette.background.default};
-        left: 50%;
-        transform: translateX(-50%);
-    }
-    .rdw-image-modal {
-        background-color: ${({ theme }) => theme.palette.background.default};
-        left: 50%;
-        transform: translateX(-50%);
-    }
-    .rdw-emoji-modal {
-        background-color: ${({ theme }) => theme.palette.background.default};
-        left: 50%;
-        transform: translateX(-50%);
-    }
-    .rdw-link-modal {
-        background-color: ${({ theme }) => theme.palette.background.default};
-        left: 0%;
-    }
-`
-
 
 const EditModal = (props) => {
 
