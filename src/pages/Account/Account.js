65325468b257207ee5f55b52000892a8e6dcbbd0
Hossam <56669856+HOSS11H@users.noreply.github.com>
2022-01-05 22:34:21 +0200

    Push

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -1,4 +1,4 @@
-import React ,  { useState, useCallback, useContext, Fragment } from 'react';
+import React, { useState, useCallback, useContext, Fragment } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
@@ -7,25 +7,28 @@ import Header from './Header/Header';
 import { Outlet } from 'react-router';
 import ThemeContext from '../../store/theme-context';
 import { connect } from 'react-redux';
+import Loader from '../../components/UI/Loader/Loader';
 
 const drawerWidth = 256;
 
-const Account = ( props ) => {
+const Account = (props) => {
+
+	const { fetchingPermissions } = props;
 
 	const themeCtx = useContext(ThemeContext)
 
 	const { theme } = themeCtx;
 
 	const customTheme = React.useMemo(
-        ( ) =>
-            createTheme({
-                ...theme,
+		() =>
+			createTheme({
+				...theme,
 				components: {
 					MuiDrawer: {
 						styleOverrides: {
 							paper: {
 								//backgroundColor: theme.palette.background.default,
-								boxShadow: 'rgb(113 122 131 / 11%) 0px 7px 30px 0px' , 
+								boxShadow: 'rgb(113 122 131 / 11%) 0px 7px 30px 0px',
 								borderRight: '0',
 							},
 						},
@@ -88,44 +91,52 @@ const Account = ( props ) => {
 						},
 					},
 				},
-            }),
-        [theme],
-    );
+			}),
+		[theme],
+	);
 
 	const [mobileOpen, setMobileOpen] = useState(false);
 
 
-	const handleDrawerToggle = useCallback( () => {
+	const handleDrawerToggle = useCallback(() => {
 		setMobileOpen(!mobileOpen);
-	} , [ mobileOpen ] ) ;
+	}, [mobileOpen]);
+	let content;
+	if (fetchingPermissions) {
+		content = <Loader height='100vh' />
+	} else {
+		content = (
+			<Box sx={{ display: 'flex', minHeight: '100vh', }}>
+				<CssBaseline />
+				<Box
+					component="nav"
+					sx={{ flexShrink: { sm: 0 }, }}
+				>
+					<Navigator
+						open={mobileOpen}
+						variant="permanent"
+					/>
+				</Box>
+				<Box sx={{ flexGrow: 1, maxWidth: mobileOpen ? `calc( 100% - ${drawerWidth}px)` : `calc( 100% - 57px)` }} >
+					<Header onDrawerToggle={handleDrawerToggle} />
+					<Box component="main" sx={{ py: 4, px: 3, bgcolor: theme.palette.background.default }}>
+						<Outlet />
+					</Box>
+				</Box>
+			</Box>
+		)
+	}
 	return (
 		<Fragment>
 			<ThemeProvider theme={customTheme}>
-				<Box sx={{ display: 'flex', minHeight: '100vh', }}>
-					<CssBaseline />
-					<Box
-						component="nav"
-						sx={{ flexShrink: { sm: 0 },  }}
-					>
-						<Navigator
-							open={mobileOpen}
-							variant="permanent"
-						/>
-					</Box>
-					<Box sx={{ flexGrow: 1, maxWidth: mobileOpen ? `calc( 100% - ${drawerWidth}px)` : `calc( 100% - 57px)` }} >
-						<Header onDrawerToggle={handleDrawerToggle} />
-						<Box component="main" sx={{ py: 4, px: 3, bgcolor: theme.palette.background.default }}>
-							<Outlet />
-						</Box>
-					</Box>
-				</Box>
+				{content}
 			</ThemeProvider>
 		</Fragment>
 	);
 }
 const mapStateToProps = (state) => {
-    return {
-        fetchedPermissions: state.permissions.permissions,
-    }
+	return {
+		fetchingPermissions: state.permissions.fetchingPermissions,
+	}
 }
 export default connect(mapStateToProps, null)(Account);
