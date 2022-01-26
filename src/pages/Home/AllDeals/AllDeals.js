2a0ac192fa46b5eeba8c43b8c5c84289d2a2eaf1
Hossam <56669856+HOSS11H@users.noreply.github.com>
2022-01-26 06:40:20 +0200

    Adding infinite scroll to all deals

--- src/pages/Home/AllDeals/AllDeals.js
+++ src/pages/Home/AllDeals/AllDeals.js
@@ -9,6 +9,8 @@ import axios from '../../../utils/axios-instance';
 import CircularProgress from '@mui/material/CircularProgress';
 import { useTranslation } from 'react-i18next';
 import HomeLayout from '../../../components/HomeLayout/HomeLayout';
+import { useRef } from 'react';
+import { useCallback } from 'react';
 
 const DealsWrapper = styled.section`
     margin: 100px 0;
@@ -23,21 +25,54 @@ const Loader = styled.div`
     width: 100%;
     height: 200px;
 `
+const Loading = styled.div`
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
 const AllDeals = props => {
 
     const { t } = useTranslation();
-    const [deals, setDeals] = useState(null);
+    const [deals, setDeals] = useState([]);
+
+    const [lastPage, setLastPage] = useState(false)
+    const [loading, setLoading] = useState(true)
+
+    // tracking on which page we currently are
+    const [page, setPage] = useState(1)
+
+
+    const ovserver = useRef()
+    const lastElementRef = useCallback((node) => {
+        if (loading) return;
+        if (ovserver.current) ovserver.current.disconnect()
+        ovserver.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && !lastPage) {
+                setPage(_page => _page + 1)
+            }
+        })
+        if (node) ovserver.current.observe(node)
+    }, [lastPage, loading])
 
     useEffect(() => {
-        axios.get('/deals?include[]=location&include[]=company')
+        setLoading(true)
+        axios.get(`/deals?include[]=location&include[]=company&page=${page}&per_page=10`)
             .then(res => {
-                setDeals(res.data.data);
+                setLoading(false)
+                setDeals(currentDeals => {
+                    return [...currentDeals, ...res.data.data]
+                });
+                if (res.data.meta.last_page === page) {
+                    setLastPage(true)
+                }
             })
             .catch(err => {
+                setLoading(false)
                 //console.log(err);
             })
-    }, [])
+    }, [page])
 
 
     let content = (
@@ -46,31 +81,46 @@ const AllDeals = props => {
         </Loader>
     );
     if (deals) {
-        content = (
-            <Grid container spacing={2}>
-                {
-                    deals.map((deal, index) => {
-                        return (
-                            <Grid item xs={6} md={4} key={deal.id}>
-                                <DealPanel deal={deal} path='../deals' />
-                            </Grid>
-                        )
-                    })
+content = (
+    <Grid container spacing={2}>
+        {
+            deals.map((deal, index) => {
+                if (deals.length === (index + 1)) {
+                    return (
+                        <Grid item xs={6} md={4} key={deal.id} ref={lastElementRef} >
+                            <DealPanel deal={deal} path='../deals' />
+                        </Grid>
+                    )
+                } else {
+                    return (
+                        <Grid item xs={6} md={4} key={deal.id}>
+                            <DealPanel deal={deal} path='../deals' />
+                        </Grid>
+                    )
                 }
+            })
+        }
+        {!lastPage && (
+            <Grid item xs={12} >
+                <Loading>
+                    <CircularProgress color="secondary" />
+                </Loading>
             </Grid>
-        );
+        )}
+    </Grid>
+);
     }
-    return (
-        <HomeLayout>
-            <DealsWrapper>
-                <Container maxWidth="lg">
-                    <Heading>
-                        <h2 className="heading-title" >{t('deals')}</h2>
-                    </Heading>
-                    {content}
-                </Container>
-            </DealsWrapper>
-        </HomeLayout>
-    )
+return (
+    <HomeLayout>
+        <DealsWrapper>
+            <Container maxWidth="lg">
+                <Heading>
+                    <h2 className="heading-title" >{t('deals')}</h2>
+                </Heading>
+                {content}
+            </Container>
+        </DealsWrapper>
+    </HomeLayout>
+)
 }
 export default AllDeals
