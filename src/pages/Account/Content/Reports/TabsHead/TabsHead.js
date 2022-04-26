2a1a449ada06c242230e2471f635ca08d1f9ae28
Hossam <56669856+HOSS11H@users.noreply.github.com>
2022-04-26 21:22:04 +0200

    upload updates

--- src/pages/Account/Content/Reports/TabsHead/TabsHead.js
+++ src/pages/Account/Content/Reports/TabsHead/TabsHead.js
@@ -2,6 +2,8 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import { useTranslation } from 'react-i18next';
+import AuthContext from '../../../../../store/auth-context';
+import { useContext } from 'react';
 
 function a11yProps(index) {
     return {
@@ -16,6 +18,8 @@ const TabsHead = props => {
 
     const { current, onChange } = props
 
+    const authCtx  = useContext(AuthContext);
+    const {roleName} = authCtx
 
     return (
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
@@ -23,7 +27,7 @@ const TabsHead = props => {
                 <Tab label={t("Tabular Report")} {...a11yProps(0)} />
                 <Tab label={t("Earning Report")} {...a11yProps(1)} />
                 <Tab label={t("Sales Report")} {...a11yProps(2)} />
-                <Tab label={t("Employee Report")} {...a11yProps(3)} />
+                {roleName !== 'artist' && <Tab label={t("Employee Report")} {...a11yProps(3)} />}
             </Tabs>
         </Box>
     )
