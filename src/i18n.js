5837828d823c241efc6a2d867f345a379d2b8953
Hossam <56669856+HOSS11H@users.noreply.github.com>
2022-08-24 01:03:36 +0200

    fixes

--- src/i18n.js
+++ src/i18n.js
@@ -668,7 +668,8 @@ const resources = {
             'you have items from another salon / artist and adding this item will remove them, do you want to procceed?': 'لديك عناصر من صالون / ارتست  أخرى وستؤدي إضافة هذا العنصر إلى إزالتها ، هل تريد المتابعة؟',
             'users': 'المستخدمين',
             'returned from booking #': 'مسترجع من حجز رقم # ',
-            'returned items': 'العناصر المسترجعة'
+            'returned items': 'العناصر المسترجعة',
+            'returns': 'المسترجع',
         }
     }
 };
